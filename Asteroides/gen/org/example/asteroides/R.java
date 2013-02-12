dd734a973f652ed59a129f5149e8e70518c5a727
tisi1988 <tisi1988@gmail.com>
2013-02-12 12:11:48 +0100

    Implementación de estilos personalizados para los botones y tema de aplicación.
    Signed-off-by: tisi1988 <tisi1988@gmail.com>

--- Asteroides/gen/org/example/asteroides/R.java
+++ Asteroides/gen/org/example/asteroides/R.java
@@ -69,5 +69,8 @@ public final class R {
  All customizations that are NOT specific to a particular API-level can go here. 
          */
         public static final int AppTheme=0x7f060001;
+        public static final int TemaAsteroides=0x7f060004;
+        public static final int TextoAsteroides=0x7f060002;
+        public static final int TextoAsteroides_Botones=0x7f060003;
     }
 }
